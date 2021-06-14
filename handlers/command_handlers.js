1ce2dffad37a1c3eaaf9fa7aceca9d93f4d3d3b9
Agistrial <73366237+Agistrial@users.noreply.github.com>
2021-06-14 12:54:19 +0700

    Update command_handlers.js

--- handlers/command_handlers.js
+++ handlers/command_handlers.js
@@ -1,1 +1,15 @@
-sa
+const fs = require('fs');
+
+mondule.export (client, Discord) => {
+    const command_files = fs.readdirSync(`./commands/`).filter(file => file.endsWith('.js'));
+    
+    for(const file of command_files){
+      const command = require('./commands/$(files)');
+      if(command_name){
+        client.commands.set(command.name, command);
+      } else {
+        continue;
+      }
+    }
+}
+  
