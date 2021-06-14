0f61782c59eb353b3ba08d05e00c7dad78d6dfea
Agistrial <73366237+Agistrial@users.noreply.github.com>
2021-06-14 11:46:20 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,4 +5,8 @@ const prefix = '-';
 client.commands = new Discord.Collection();
 client.events = new Discord.Collection();
 
+['command_handlers', 'event_handlers'].forEach(handler =>{
+    require('.handlers/${handler})(client, Discord);
+})
+
 client.login(process.env.TOKEN);
